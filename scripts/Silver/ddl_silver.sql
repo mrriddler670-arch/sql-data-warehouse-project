d680e6492d08d66a1f52528bed1324711cb1b226
mrriddler670-arch <mrriddler670@gmail.com>
2026-02-01 17:45:58 +0530

    ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -6,7 +6,7 @@ Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
+==============================================================================
 */
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
